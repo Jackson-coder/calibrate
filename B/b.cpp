7e3084da14a2dbb774afdf47bbc886b67a81f43c
Jackson-coder <jlxxlyh@163.com>
2021-11-11 11:25:06 +0800

    single camera calibration

---
+++ B/b.cpp
@@ -0,0 +1,6 @@
+#include "b.h"
+int coutt_2()
+{
+    coutt_1();
+    printf("2");
+}
---
+++ B/b.h
@@ -0,0 +1,5 @@
+#pragma B_H
+#include <iostream>
+#include "calibration.h"
+using namespace std;
+int coutt_2();
